cf5ef16d0403f0428e8275af4ee58c12499d9219
Pavel Galeev <galeevpavel@gmail.com>
2023-02-09 10:12:14 +0300

    ЗАДАНИЕ 12.7.3 (HW-03)

--- 12.7.3.py
+++ 12.7.3.py
@@ -1,1 +1,10 @@
-11
+money = int(input("Сумма вклада: "))
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+deposit = []
+deposit.append(money*per_cent['ТКБ']/100)
+deposit.append(money*per_cent['СКБ']/100)
+deposit.append(money*per_cent['ВТБ']/100)
+deposit.append(money*per_cent['СБЕР']/100)
+i = round(max(deposit),3)
+
+print("Максимальная сумма, которую вы можете заработать:", i)
