79d1b5acdb7619f9ca2e97dc49fc5fb3598fef50
Pavel <120167171+GaleevPavel@users.noreply.github.com>
2023-03-08 17:15:56 +0300

    19.7.2 petfriends.skillfactory.ru

---
+++ 19.7.2/README.md
@@ -0,0 +1,2 @@
+# Tests REST API
+Тест-кейсы для REST API интерфейса PetFriends API v1 (https://petfriends.skillfactory.ru/)
---
+++ 19.7.2/settings.py
@@ -0,0 +1,6 @@
+valid_email = "name555@mail.ru"
+valid_password = "name555"
+empty_email = ""
+empty_password = ""
+empty_auth_key = {"key": ""}
+incorrect_auth_key = {"key": "1234567890qwerty"}
