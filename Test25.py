c34aac8b4c67f6b63a04060f6d34b1097ca65f63
Pavel <120167171+GaleevPavel@users.noreply.github.com>
2023-04-01 14:09:57 +0300

    25.5.1HW

---
+++ Test25.py
@@ -0,0 +1,45 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+@pytest.fixture(autouse=True)
+def testing():
+    pytest.driver = webdriver.Chrome('./chromedriver.exe')
+    pytest.driver.get('https://petfriends.skillfactory.ru/login')
+    yield
+    pytest.driver.quit()
+
+def test_show_my_pets():
+    pytest.driver.find_element(By.ID, 'email').send_keys('name555@mail.ru')
+
+    # Вводим пароль
+    pytest.driver.find_element(By.ID, 'pass').send_keys('name555')
+
+    # Нажимаем на кнопку входа в аккаунт
+    pytest.driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
+
+    # Проверяем, что мы оказались на главной странице пользователя
+    assert pytest.driver.find_element(By.TAG_NAME, 'h1').text == "PetFriends"
+
+    # неявное ожидание
+    pytest.driver.implicitly_wait(10)
+
+    # явное ожидание
+    WebDriverWait(pytest.driver, 10).until(EC.presence_of_element_located((By.XPATH, "//a[contains(text(),'Мои питомцы')]")))
+
+
+    images = WebDriverWait(pytest.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.card-deck .card-img-top')))
+    names = WebDriverWait(pytest.driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.card-deck .card-title')))
+    descriptions = WebDriverWait(pytest.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.card-deck .card-text')))
+
+
+    for i in range(len(names)):
+        assert images[i].get_attribute('src') != ''
+        assert names[i].text != ''
+        assert descriptions[i].text != ''
+        assert ', ' in descriptions[i]
+        parts = descriptions[i].text.split(", ")
+        assert len(parts[0]) > 0
+        assert len(parts[1]) > 0
